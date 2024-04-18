9d8951f03692edeaf7d1044fb3b3738312b88b29
Sh6abrA <47980718+Sh6abrA@users.noreply.github.com>
2024-04-18 08:51:45 +0500

    Upgrade Sidebar

---
+++ src/shared/ui/List/List.tsx
@@ -0,0 +1,27 @@
+import classNames from 'classnames';
+import * as cls from './List.module.scss';
+import { Button, ThemeButton } from '../Button/Button';
+
+interface ListProps {
+    className?: string
+}
+
+export const List = ({ className }: ListProps) => {
+    return (
+        <ul className={cls.List}>
+            <li className={cls.listItem}>
+                <Button theme={ThemeButton.SECONDARY} className={"newChatBtn"}>Чат</Button>
+            </li>
+            <li className={cls.listItem}>
+                <Button theme={ThemeButton.SECONDARY} className={"newChatBtn"}>Чат</Button>
+            </li>
+            <li className={cls.listItem}>
+                <Button theme={ThemeButton.SECONDARY} className={"newChatBtn"}>Чат</Button>
+            </li>
+            <li className={cls.listItem}>
+                <Button theme={ThemeButton.SECONDARY} className={"newChatBtn"}>Чат</Button>
+            </li>
+        </ul>
+    );
+}
+
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -2,6 +2,7 @@ import classNames  from 'classnames';
 import * as cls from './Sidebar.module.scss';
 import { Logo } from 'shared/ui/Logo/Logo';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { List } from 'shared/ui/List/List';
 
 interface SidebarProps {
     className?: string
@@ -11,7 +12,8 @@ export const Sidebar = ({className}: SidebarProps) => {
     return (
         <div className={classNames(cls.Sidebar, {}, [className])}>
             <Logo/>
-            <Button theme={ThemeButton.SECONDARY} className={"newChatBtn"}>Создать новый</Button>
+            <Button theme={ThemeButton.SECONDARY} className={"newChatBtn"}>Создать новый чат</Button>
+            <List/>
         </div>
     );
 }
